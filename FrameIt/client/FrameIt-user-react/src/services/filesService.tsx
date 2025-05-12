23baca762effdccf45a5e37d93937a2544c581b2
Rachel02247 <rachel02247@gmail.com>
2025-05-13 01:59:25 +0300

    preview works!

--- FrameIt/client/FrameIt-user-react/src/services/filesService.tsx
+++ FrameIt/client/FrameIt-user-react/src/services/filesService.tsx
@@ -33,9 +33,10 @@ export const getFileDownloadUrl = async (s3Key: string) => {
     console.log("in getFileDownloadUrl before server", s3Key);
     
     const response = await axios.get(`${API_URL_BASE}/${s3Key}/download`);
-    console.log("res after server", response);
+    console.log("res after server", response.data);
     
-    return response.data.url; // Assuming the API returns a `url` field
+    // return response.data.url; // Assuming the API returns a `url` field
+    return response.data; // Assuming the API returns a `url` field
   } catch (error) {
     console.error("Error fetching file download URL:", error);
     throw error;
