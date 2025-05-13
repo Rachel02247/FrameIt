e01c501387b3e3b4ecb0d16917368f834e913f8d
LeahPosen <149408800+LeahPosen@users.noreply.github.com>
2025-05-13 15:43:44 +0300

    mark as comment

--- FrameIt/client/FrameIt-user-react/src/page/gallery/fileItem.tsx
+++ FrameIt/client/FrameIt-user-react/src/page/gallery/fileItem.tsx
@@ -67,36 +67,36 @@ const FileItem: React.FC<FileItemProps> = ({ file, onDelete, onOpenPreview }) =>
 
   const isVideo = file.fileType.toLowerCase() === "mp4" || file.fileType.toLowerCase() === "mov"
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    const loadFileUrl = async () => {
+  //   const loadFileUrl = async () => {
 
-      setIsLoading(true)
+  //     setIsLoading(true)
 
-      try {
+  //     try {
 
-        const result = await dispatch(getFileDownloadUrl(file.s3Key)).unwrap();
-        setPresignedUrl(result);
+  //       const result = await dispatch(getFileDownloadUrl(file.s3Key)).unwrap();
+  //       setPresignedUrl(result);
 
-      } catch (error) {
+  //     } catch (error) {
         
-        console.error("Error loading file URL:", error)
-        setImageError(true)
+  //       console.error("Error loading file URL:", error)
+  //       setImageError(true)
       
-      } finally {
-        setIsLoading(false)
-      }
-    }
-    if (file.s3Key) {
-      loadFileUrl();
-    }
-  }, [file.s3Key, dispatch])
+  //     } finally {
+  //       setIsLoading(false)
+  //     }
+  //   }
+  //   if (file.s3Key) {
+  //     loadFileUrl();
+  //   }
+  // }, [file.s3Key, dispatch])
 
-  useEffect(() => {
-    if (userId) {
-      dispatch(fetchUserCollections(userId))
-    }
-  }, [userId, dispatch])
+  // useEffect(() => {
+  //   if (userId) {
+  //     dispatch(fetchUserCollections(userId))
+  //   }
+  // }, [userId, dispatch])
 
   const handleOpenMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation(); 
