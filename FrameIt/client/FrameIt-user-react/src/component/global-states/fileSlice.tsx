cd92a68bc54556c76a75cb721017305f24509de0
Rachel02247 <rachel02247@gmail.com>
2025-05-13 14:30:26 +0300

    allow multiple uploads

--- FrameIt/client/FrameIt-user-react/src/component/global-states/fileSlice.tsx
+++ FrameIt/client/FrameIt-user-react/src/component/global-states/fileSlice.tsx
@@ -52,7 +52,12 @@ export const getFileDownloadUrl = createAsyncThunk(
   "files/getFileDownloadUrl",
   async (s3Key: string, { rejectWithValue }) => {
     try {
+      console.log("bfor gtFileDownloadUrl");
+      
       const response = await axios.get(`${API_URL_BASE}/${s3Key}/download`);
+      console.log("in getFileDownloadUrl");
+      console.log(response.data);
+      
       return response.data;
     } catch (error) {
       console.error("Error fetching file download URL:", error);
